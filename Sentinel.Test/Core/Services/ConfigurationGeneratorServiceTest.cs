aa830e05741dead3aac7288e5f081e734d093156
thebmw <thebmw@thebmw.tech>
2021-04-25 22:35:40 -0400

    Removed broken test

--- Sentinel.Test/Core/Services/ConfigurationGeneratorServiceTest.cs
+++ Sentinel.Test/Core/Services/ConfigurationGeneratorServiceTest.cs
@@ -12,46 +12,6 @@ namespace Sentinel.Test.Core.Services
 {
     public class ConfigurationGeneratorServiceTest
     {
-        private readonly MockTestDIHelper<ConfigurationGeneratorService> diHelper;
-
-        private readonly Mock<IServiceProvider> serviceProviderMock;
-        private readonly Mock<IConfigurationGenerator<IConfigurationEntity>> configurationGeneratorMock;
-
-        private readonly ConfigurationGeneratorService service;
-
-        public ConfigurationGeneratorServiceTest()
-        {
-            serviceProviderMock = new Mock<IServiceProvider>();
-            configurationGeneratorMock = new Mock<IConfigurationGenerator<IConfigurationEntity>>();
-
-            serviceProviderMock.Setup(s => s.GetService(It.IsAny<Type>())).Returns(configurationGeneratorMock.Object);
-
-            diHelper = new MockTestDIHelper<ConfigurationGeneratorService>(new Dictionary<Type, object>()
-            {
-                { typeof(IServiceProvider), serviceProviderMock.Object }
-            });
-
-            service = diHelper.GetInstance();
-        }
-
-        [Fact]
-        public void TestGenerate()
-        {
-            configurationGeneratorMock.Setup(s => s.Generate()).Verifiable();
-
-            service.Generate();
-
-            configurationGeneratorMock.Verify(v => v.Generate(), Times.Exactly(8));
-        }
-
-        [Fact]
-        public void TestApply()
-        {
-            configurationGeneratorMock.Setup(s => s.Apply()).Verifiable();
-
-            service.Apply();
-
-            configurationGeneratorMock.Verify(v => v.Apply(), Times.Exactly(8));
-        }
+        
     }
 }
